9b431f37d830685a7bdd3db96ec05b5e906e404f
Nahin-Yassin <nahinyasin32@gmail.com>
2023-06-22 01:10:28 +0600

    Next 5days temp update

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ const apiKey = `3cafaa59f1bb4719ba983312232006`;
 //Get Location Temperature by Async Request
 async function getWeatherData(cityName) {
     try {
-        const apiUrl = `//api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no`;
+        const apiUrl = `//api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&days=5&aqi=no`;
         const response = await fetch(apiUrl);
 
         if (response.ok) {
             const data = await response.json();
-            getWeatherDataContent(data.location.country, data.location.name, data.current.condition.icon, data.current.temp_c, data.current.humidity, data.current.condition.text, false);
+
+            let { location: { country }, location: { name }, current: { condition: { icon } }, current: { temp_c }, current: { humidity }, current: { condition: { text } }, forecast: { forecastday } } = data
+            getWeatherDataContent(country, name, icon, temp_c, humidity, text, false)
+            getWeatherDataforecast(forecastday)
         } else {
             let text = `
                 <div class="alert alert-danger" role="alert">
@@ -28,7 +31,7 @@ async function getWeatherData(cityName) {
 // Current Location using XMLHttp Request
 const currentIpLocation = (position) => {
     const latLong = position.coords.latitude + ',' + position.coords.longitude
-    const apiUrl = `//api.weatherapi.com/v1/current.json?key=${apiKey}&q=${latLong}&aqi=no`
+    const apiUrl = `//api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${latLong}&days=5&aqi=no`
 
     const Http = new XMLHttpRequest();
     Http.open("GET", apiUrl);
@@ -36,8 +39,10 @@ const currentIpLocation = (position) => {
     Http.onreadystatechange = (e) => {
         if (Http.readyState === 4 && Http.status === 200) {
             const data = JSON.parse(Http.responseText);
-            let browserLocation = true;
-            getWeatherDataContent(data.location.country, data.location.name, data.current.condition.icon, data.current.temp_c, data.current.humidity, data.current.condition.text, browserLocation)
+            let { location: { country }, location: { name }, current: { condition: { icon } }, current: { temp_c }, current: { humidity }, current: { condition: { text } }, forecast: { forecastday } } = data
+
+            getWeatherDataContent(country, name, icon, temp_c, humidity, text, true)
+            getWeatherDataforecast(forecastday)
         }
     }
 };
@@ -49,6 +54,7 @@ navigator.geolocation.getCurrentPosition(currentIpLocation, errorCallback);
 
 // Content Update by Parameters
 getWeatherDataContent = (countyName, City, img, temp, humidity, text, browserLocation) => {
+    console.log(browserLocation);
     let content = `
     <div class="location"> ${browserLocation ? 'Current location: ' : 'Location: '} ${countyName + ', ' + City}</div>
     <div class="temperature">
@@ -61,13 +67,35 @@ getWeatherDataContent = (countyName, City, img, temp, humidity, text, browserLoc
         <span class="description">Condition: <span>${text}</span></span>
     </div>`;
     document.getElementById("tempSection").innerHTML = content;
-    return content
 }
 
+// Show Next 5 Days Content
+getWeatherDataforecast = (futureContent) => {
+    let forecast = '';
+    for (let futureC of futureContent) {
+        // Date to day convert 
+        const dateStr = futureC.date;
+        const date = new Date(dateStr);
+        const options = { weekday: 'long' };
+        const dayName = new Intl.DateTimeFormat('en-US', options).format(date);
+
+        forecast += `
+        <div class="weather-card mobile">
+            <div class="day">${dayName}</div>
+            <img src="${futureC.day.condition.icon}" alt="Weather Icon">
+            <div class="temperatures">
+                <span class="high">${futureC.day.maxtemp_c}°C</span>
+                <span class="low">${futureC.day.mintemp_c}°C</span>
+            </div>
+        </div>`;
+    }
+    document.getElementById("futureTemp").innerHTML = forecast;
+}
 
 // Form Handling by Submit Event
 form.addEventListener("submit", function (e) {
     e.preventDefault();
+    //const data = new FormData(form);
     let cityName = search.value
     getWeatherData(cityName)
 })
